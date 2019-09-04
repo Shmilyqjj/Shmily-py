33d1f936a6a963ffe6f1ed0a3861a9a81f168434
Shmilyqjj <710552907@qq.com>
2019-09-05 00:20:07 +0800

    Python应用

---
+++ Python_Applications/Logs_Manager.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+:Description: 
+:Owner: jiajing_qu
+:Create time: 2019/8/19
+"""
+
+import os
+import datetime
+import time
+import shutil
+LOG_FILE_PATH = '/data/logs/streaming_monitor/monitor.log'
+HISTORY_LOG_PATH = '/data/logs/streaming_monitor/history'
+LOG_DIR_PATH = '/data/logs/streaming_monitor'
+MAX_LOG_SIZE = 10485760   # 10M
+
+
+def logs_manager():
+    """
+    管理日志文件（自动清理）
+    日志文件位置LOG_FILE_PATH = '/data/logs/streaming_monitor/monitor.log'
+    达到10M时移入HISTORY_LOG_PATH = '/data/logs/streaming_monitor/history'
+    七天后删除HISTORY_LOG
+    """
+    now_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    mkdir(LOG_DIR_PATH)
+    if os.path.exists(LOG_FILE_PATH):
+        file_size = os.path.getsize(LOG_FILE_PATH)
+        if file_size >= MAX_LOG_SIZE:
+            shutil.move(LOG_FILE_PATH, '%s/%s_monitor.log' % (HISTORY_LOG_PATH, now_time))
+            print('日志达到%d字节，移入%s' % (MAX_LOG_SIZE, HISTORY_LOG_PATH))
+    if len(os.listdir(HISTORY_LOG_PATH)) != 0:  # 文件数不为0
+        for name in os.listdir(HISTORY_LOG_PATH):
+            now_timestamp = time.time()
+            alter_timestamp = os.path.getmtime('%s/%s' % (HISTORY_LOG_PATH, name))
+            if now_timestamp - alter_timestamp >= 604800:
+                os.remove('%s/%s' % (HISTORY_LOG_PATH, name))
+                print('历史日志文件过期,删除%s文件' % name)
---
+++ Python_Applications/find_package_name.py
@@ -0,0 +1,350 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+:Description: 
+:Owner: jiajing_qu
+:Create time: 2019/8/22
+"""
+import os
+import re
+from collections import deque  # List插入慢 用deque优化插入删除效率
+
+
+PYTHON_INNER_LIB = ['abc',
+'aifc',
+'antigravity',
+'anydbm',
+'argparse',
+'ast',
+'asynchat',
+'asyncore',
+'select',
+'atexit',
+'audiodev',
+'base64',
+'BaseHTTPServer',
+'Bastion',
+'bdb',
+'binhex',
+'bisect',
+'bsddb',
+'calendar',
+'cgi',
+'CGIHTTPServer',
+'cgitb',
+'chunk',
+'cmd',
+'code',
+'codecs',
+'codeop',
+'collections',
+'colorsys',
+'commands',
+'compileall',
+'compiler',
+'ConfigParser',
+'contextlib',
+'Cookie',
+'cookielib',
+'copy',
+'client',
+'copy_reg',
+'cProfile',
+'csv',
+'ctypes',
+'curses',
+'dbhash',
+'decimal',
+'difflib',
+'dircache',
+'dis',
+'distutils',
+'doctest',
+'desensitization_func_c',
+'desensitization_spark_udf_c',
+'pyspark',
+'tesla',
+'math',
+'random',
+'datetime',
+'DocXMLRPCServer',
+'dumbdbm',
+'dummy_thread',
+'dummy_threading',
+'email',
+'encodings',
+'filecmp',
+'fileinput',
+'fnmatch',
+'formatter',
+'fpformat',
+'fractions',
+'ftplib',
+'functools',
+'genericpath',
+'getopt',
+'getpass',
+'gettext',
+'glob',
+'gzip',
+'hashlib',
+'heapq',
+'hmac',
+'hotshot',
+'htmlentitydefs',
+'htmllib',
+'HTMLParser',
+'httplib',
+'idlelib',
+'ihooks',
+'imaplib',
+'imghdr',
+'importlib',
+'imputil',
+'inspect',
+'io',
+'json',
+'keyword',
+'lib-tk',
+'lib2to3',
+'linecache',
+'locale',
+'logging',
+'macpath',
+'macurl2path',
+'mailbox',
+'mailcap',
+'markupbase',
+'md5',
+'mhlib',
+'mimetools',
+'mimetypes',
+'MimeWriter',
+'mimify',
+'modulefinder',
+'msilib',
+'multifile',
+'multiprocessing',
+'mutex',
+'netrc',
+'new',
+'nntplib',
+'ntpath',
+'nturl2path',
+'numbers',
+'oci.dll',
+'opcode',
+'optparse',
+'operator',
+'orannzsbb10.dll',
+'oraociicus10.dll',
+'os',
+'os2emxpath',
+'pdb',
+'pickle',
+'pickletools',
+'pipes',
+'pkgutil',
+'platform',
+'plistlib',
+'popen2',
+'poplib',
+'posixfile',
+'posixpath',
+'pprint',
+'profile',
+'pstats',
+'pty',
+'lbr',
+'pydoc',
+'pydoc_data',
+'py_compile',
+'Queue',
+'quopri',
+'random',
+'re',
+'requests',
+'repr',
+'rexec',
+'rfc822',
+'rlcompleter',
+'robotparser',
+'runpy',
+'sched',
+'sets',
+'sgmllib',
+'sha',
+'shelve',
+'shlex',
+'shutil',
+'SimpleHTTPServer',
+'SimpleXMLRPCServer',
+'service',
+'select',
+'pysqlContext ',
+'site-packages',
+'site',
+'smtpd',
+'smtplib',
+'sndhdr',
+'socket',
+'sys',
+'SocketServer',
+'sqlite3',
+'sre',
+'sre_compile',
+'sre_constants',
+'sre_parse',
+'ssl',
+'stat',
+'statvfs',
+'string',
+'StringIO',
+'stringold',
+'stringprep',
+'struct',
+'subprocess',
+'sunau',
+'sunaudio',
+'symbol',
+'symtable',
+'sysconfig',
+'tabnanny',
+'tarfile',
+'telnetlib',
+'tempfile',
+'test',
+'textwrap',
+'this',
+'threading',
+'timeit',
+'toaiff',
+'token',
+'tokenize',
+'trace',
+'traceback',
+'tty',
+'types',
+'tornado',
+'time',
+'unittest',
+'urllib',
+'urllib2',
+'urlparse',
+'user',
+'UserDict',
+'UserList',
+'UserString',
+'uu',
+'uuid',
+'itertools',
+'warnings',
+'wave',
+'weakref',
+'webbrowser',
+'whichdb',
+'wsgiref',
+'xdrlib',
+'xml',
+'xmllib',
+'airflow',
+'NER_CompanyAndPerson',
+'xmlrpclib',
+'zipfile',
+'_abcoll',
+'_LWPCookieJar',
+'_MozillaCookieJar',
+'_osx_support',
+'_pyio',
+'_strptime',
+'_threading_local',
+'_weakrefset',
+'__future__',
+'__phello__.foo',
+'datetime',
+'email',
+'html',
+'http',
+'queue',
+'misc',
+'socket',
+'socketserver',
+'test',
+'total_ordering',
+'urllib',
+'xmlrpc',
+'_markupbase']
+
+
+def find_package_name(project_path):
+    """
+    找到所有包/库的名称，存入无重复set集合
+    :param project_path:
+    :return:
+    """
+    all_pack_name = set()   # s.add(package_name)
+    if os.path.exists(project_path):
+        for root, dirs, filenames in os.walk(project_path):
+            path = [os.path.join(root, name) for name in filenames]
+            for files in path:
+                if files:
+                    if re.match('.*py$', files):
+                        # print(files)
+                        with open(files, 'r') as f:
+                            lines = f.readlines()
+                            for line in lines:
+                                if re.match('^\s*import.*', line) or re.match('^\s*from.*import.*', line):
+                                    line = line
+                                    package_list = line.split(' ')[1].split('.')[0].strip().strip('*').strip('=').split(',')
+                                    if len(package_list) == 1:
+                                        all_pack_name.add(package_list[0])
+                                    else:
+                                        for pack in package_list:
+                                            all_pack_name.add(pack)
+    for pack_name in all_pack_name:
+        if pack_name not in PYTHON_INNER_LIB and pack_name not in find_file_name(project_path):
+            print(pack_name)
+
+
+
+
+# def find_file_name(project_path):
+#     """
+#     所有py文件的文件名
+#     :param project_path: 项目文件夹路径
+#     :return: file_name_list 所有文件名 list
+#     """
+#     file_name_list = []
+#     if os.path.exists(project_path):
+#         for root, dirs, filenames in os.walk(project_path):
+#             for files in filenames:
+#                 if files:
+#                     if re.match('.*py$', files): # 正确
+#                         files = files.rstrip('.py')
+#                         file_name_list.append(files)
+#     return file_name_list
+
+
+def find_file_name(project_path):
+    """
+    所有py文件的文件名
+    :param project_path: 项目文件夹路径
+    :return: file_name_list 所有文件名 list
+    """
+    file_name_list = deque()
+    if os.path.exists(project_path):
+        for root, dirs, filenames in os.walk(project_path):
+            for files in filenames:
+                if files:
+                    if re.match('.*py$', files): # 正确
+                        files = files.rstrip('.py')
+                        file_name_list.append(files)
+    return file_name_list
+
+
+if __name__ == '__main__':
+    project_path = r'D:\svn\tesla_branch\tesla'
+    find_package_name(project_path)
+    # s = find_file_name(project_path)
+    # print(s)
