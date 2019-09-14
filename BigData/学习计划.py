2330aa1132592ee7bebb9bad82c36b347183b59e
Shmilyqjj <710552907@qq.com>
2019-09-14 10:09:56 +0800

    大数据学习计划

---
+++ BigData/学习计划.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+"""
+Description:
+Author:曲佳境
+Date: 2019/9/14 10:07
+"""
+
+"""
+week_1
+airflow相关学习       官网、tesla代码（job_dags_new）   已沟通     1天
+spark2.4学习，        官网相关参数了解                  已沟通     2天
+phoenix相关使用       网上教程                          已沟通     1天
+hbase导数流程优化     付同配合                          已沟通     1天
+
+week_2
+
+
+
+长期（2019年）：
+09月份：airflow熟悉、spark可通过官网自己编写所有代码、Alluxio调研
+10月份：kafka熟悉、zeppelin熟悉使用及简单运维
+11月份：nifi开发可配置、kylin熟悉会用、hbase熟悉
+12月份：persto原理、flink、atlas熟悉操作
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+old:
+09月份：airflow熟悉、spark可通过官网自己编写所有代码、zeppelin熟悉使用及简单运维
+10月份：nifi开发可配置、kylin熟悉会用、hbase熟悉
+11月份：persto原理、flink、atlas熟悉操作
+12月份：kafka熟悉、Alluxio调研
+"""
