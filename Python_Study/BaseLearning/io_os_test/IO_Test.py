3cf0f78de1b3ea9ec6d3b2dff79527a040ec7121
jiajing_qu <710552907@qq.com>
2019-09-23 16:44:16 +0800

    IO/OS 测试

--- Python_Study/BaseLearning/io_os_test/IO_Test.py
+++ Python_Study/BaseLearning/io_os_test/IO_Test.py
@@ -2,6 +2,9 @@
 """
 IO测试 文件读写
 """
+import os
+
+
 def inputTest():
     str = raw_input()    # 函数从标准输入读取一个行，并返回一个字符串（去掉结尾的换行符）
     print(str)
@@ -9,6 +12,10 @@ def inputTest():
     print(str)
 
 def File_IO():
+    """
+    文件读写
+    :return:
+    """
     file  = open("d:\\IO_TEST.txt",'a+')
     print(file.name)
     print(file.closed)
@@ -17,9 +24,9 @@ def File_IO():
     file.close()
 
     file0 = open("d:\\Program Files\\hadoop-2.6.0\\etc\\hadoop\\hdfs-site.xml",'a+')
-    print(file0.read())
-    print(file0.readline())
-    print(file0.readlines())
+    print(file0.read()) # 读取整个文件，将文件内容放到一个字符串变量中。 如果文件非常大，尤其是大于内存时，无法使用read()方法。
+    print(file0.readline()) # 每次只读取一行 与while True和else break连用
+    print(file0.readlines()) # 一次性读取整个文件；自动将文件内容分析成一个行的列表list格式
 
     #os.rename()
     #os.remove()
@@ -30,6 +37,16 @@ def main():
     print("exec main")
     # inputTest()
     File_IO()
+
+
+def read_dir(path):
+    """
+    遍历文件夹下所有文件
+    :param path:
+    :return:
+    """
+    for root, dirs, filenames in os.walk(path):
+        path = [os.path.join(root, name) for name in filenames] # 所有文件全路径
 
 if __name__ == "__main__":
     main()
