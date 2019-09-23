3788ece505fd1423cdb1aba9a39a3bf6f9cb0588
jiajing_qu <710552907@qq.com>
2019-09-23 21:33:55 +0800

    AirFlow应用-实现全局Clear指定的task及有关依赖

--- Python_Applications/AirFlow_Applications/global_clear_airflow.py
+++ Python_Applications/AirFlow_Applications/global_clear_airflow.py
@@ -7,6 +7,7 @@
 :Create time: 2019/9/19 16:39
 """
 import commands
+
 import datetime
 from airflow import models
 from airflow import settings
@@ -108,22 +109,26 @@ def main(para = None):
     task_id = para.get('task_id')
     dep_list = get_dependencies(dag_id, task_id, down=True)
     date = (datetime.datetime.now()+datetime.timedelta(days=-1)).strftime("%Y-%m-%d")  # 当前日期减一天
-    # 先clear该dag内的dep_list
-    for task in dep_list:
-        # airflow clear daily_task_online -t time.time_sensor_08_00 -d -s 2019-09-21 -e 2019-09-21
-        (status, output) = commands.getstatusoutput('airflow clear %s -t %s -d -s %s -e %s' % (dag_id, task, date, date))
-        if status != 0:
-            print(output)
+    # 先clear该task
+    # airflow clear daily_task_online -t time.time_sensor_08_00 -d -s 2019-09-21 -e 2019-09-21
+    cmd = 'airflow clear %s -t %s -d -c -s %s -e %s' % (dag_id, task_id, date, date)
+    (status, output) = commands.getstatusoutput(cmd)
+    print(cmd)
+    if status != 0:
+        print(output)
     whole_dag_tasks = find_whole_tasks_and_dag()
     clear_tasks.clear()
-    find_related_tasks(dep_list, whole_dag_tasks)
+    find_related_tasks(dep_list, whole_dag_tasks)   # 有关的sensor列表保存再clear_tasks
     # 再clear与_sensor相关的clear_tasks
     for dag in clear_tasks:
         for task in clear_tasks[dag]:
             # airflow clear daily_task_online -t time.time_sensor_08_00 -d -s 2019-09-21 -e 2019-09-21
-            (status, output) = commands.getstatusoutput('airflow clear %s -t %s -d -s %s -e %s' % (dag, task, date, date))
+            cmd = 'airflow clear %s -t %s -d -c -s %s -e %s' % (dag, task, date, date)
+            (status, output) = commands.getstatusoutput(cmd)
+            print(cmd)
             if status != 0:
                 print(output)
+
 
 
 if __name__ == '__main__':
