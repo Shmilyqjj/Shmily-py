6d2076e53dc7248cab060c22c8f87b9c224ef413
“Shmilyqjj” <710552907@qq.com>
2019-11-01 19:33:22 +0800

    MongoDB 工具类

---
+++ BigData/learn_and_tests/mongo/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+:Description: MongoDB 工具类和测试
+:Owner: jiajing_qu
+:Create time: 2019/11/1 14:04
+"""
---
+++ BigData/learn_and_tests/mongo/mongo_util.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+:Description: MongoDB 工具类
+:Owner: jiajing_qu
+:Create time: 2019/11/1 14:04
+"""
+import pymongo
+
+class mongo_util:
+    """
+    MongoDB 工具类
+    """
+    def __init__(self, host, port):
+        self._client = pymongo.MongoClient(host=host, port=port)
+
