9a90c7f3ee805840bed2d1becef9f338cf2e4ba4
Shmilyqjj <710552907@qq.com>
2019-12-16 01:07:50 +0800

    毕设项目->影视平台大数据分析与可视化系统

---
+++ BigData/projects/BigdataAnalysisAndVisualizationSystem/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+"""
+Description: 影视平台大数据分析与可视化系统
+Author:曲佳境
+Date: 2019/12/13 23:28
+"""
+
+"""
+影视平台大数据分析与可视化系统
+语言：Python + Java + Scala + Shell
+技术栈：Hadoop + Spark + Zookeeper + ClouderaManager + Kafka + Flume + HBase + AirFlow + Alluxio + Mysql + Hive + Phoenix
+"""
