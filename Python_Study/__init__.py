153b14e8f33c9dfa395decdc9f86b5dba45d3a04
“Shmilyqjj” <710552907@qq.com>
2019-12-30 19:02:39 +0800

    inint

--- Python_Study/__init__.py
+++ Python_Study/__init__.py
@@ -1,5 +1,5 @@
-"""
-pip 下载module的时候加参数使用国内pip源，速度快
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple module_name
-example: pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pymysql
-"""
+# """
+# use faster source
+# pip install -i https://pypi.tuna.tsinghua.edu.cn/simple module_name
+# example: pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pymysql
+# """
