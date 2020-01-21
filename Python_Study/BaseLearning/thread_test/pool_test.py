6f9d0c7bb30db6a049945f4e0af383b17413ed0a
“Shmilyqjj” <710552907@qq.com>
2020-01-21 16:26:23 +0800

    pool test

--- Python_Study/BaseLearning/thread_test/pool_test.py
+++ Python_Study/BaseLearning/thread_test/pool_test.py
@@ -5,6 +5,9 @@
 :Description: thread pool加速for循环中的操作
 :Owner: jiajing_qu
 :Create time: 2019/12/31 10:53
+
+multiprocessing包是Python中的多进程管理包。它与 threading.Thread类似，可以利用multiprocessing.Process对象来创建一个进程。该进程可以允许放在Python程序内部编写的函数中。该Process对象与Thread对象的用法相同，拥有is_alive()、join([timeout])、run()、start()、terminate()等方法。
+这个模块表示像线程一样管理进程，这个是multiprocessing的核心，它与threading很相似，对多核CPU的利用率会比threading好的多。
 """
 
 from multiprocessing.pool import ThreadPool
