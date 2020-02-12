5b70f7c5822822674de65e0e108eca75348d9ae0
Shmilyqjj <710552907@qq.com>
2020-02-12 20:20:56 +0800

    os test

---
+++ Python_Study/BaseLearning/io_os_sys_test/os_test.py
@@ -0,0 +1,12 @@
+"""
+:Author: jiajing_qu
+:Create Time: 2020/2/12 19:57
+:@File: os_test.py
+:@Software: PyCharm
+:@Site: shmily-qjj.top
+"""
+import os
+print(os.getcwd())
+print(os.getcwdu())
+print(os.chdir("C:\Users\Home-PC\PycharmProjects\Shmily-py\Python_Study\BaseLearning\io_os_sys_test"))  #  改变工作路径
+
