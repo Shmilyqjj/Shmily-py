8c0cc2f0b4f10eb1159a489d2f8dd263ccd0d8bb
Shmilyqjj <710552907@qq.com>
2020-02-12 19:55:59 +0800

    脚本模板

--- Python_Study/__init__.py
+++ Python_Study/__init__.py
@@ -1,5 +1,18 @@
-# """
-# use faster source
-# pip install -i https://pypi.tuna.tsinghua.edu.cn/simple module_name
-# example: pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pymysql
-# """
+"""
+use faster source
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple module_name
+example: pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pymysql
+"""
+
+
+#####################################
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+:Author: jiajing_qu
+:Create Time: ${DATE} ${TIME}
+:@File: ${NAME}.py
+:@Software: ${PRODUCT_NAME}
+:@Site: shmily-qjj.top
+"""
+######################################
