139686289696da80f36aa9f7707cc3e0395833b6
Shmilyqjj <710552907@qq.com>
2020-02-16 20:43:24 +0800

    airflow的全局clear

--- Python_Applications/AirFlow_Applications/global_clear_airflow.py
+++ Python_Applications/AirFlow_Applications/global_clear_airflow.py
@@ -2,15 +2,18 @@
 # -*- coding:utf-8 -*-
 
 """
-:Description: 获取AirFlow的tasks的上游和下游,实现airflow的全局clear
+:Description: 获取AirFlow的tasks的上游和下游以及sensor相关tasks,实现airflow的全局clear
 :Owner: jiajing_qu
 :Create time: 2019/9/19 16:39
 """
 import commands
-
 import datetime
 from airflow import models
 from airflow import settings
+from tesla.common.utility.para_deal import auto_batch_original
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 dag_bag = models.DagBag(settings.DAGS_FOLDER)
 results = set()  # 用于暂存结果
 clear_tasks = {}
@@ -102,19 +105,43 @@ def find_related_tasks(dep_list, whole_dag_tasks):
                 if not ''.endswith("_sensor"):  # 如果_sensor结尾的task,忽略
                     find_related_tasks(next_dep_list, whole_dag_tasks)
 
-@auto_batch2()
-def main(para = None):
+
+def run_and_return(cmd, dag, task):
+    """
+    运行airflow clear命令并打印结果
+    :param cmd:  airflow clear命令
+    :return:
+    """
+    (status, output) = commands.getstatusoutput(cmd)
+    if status != 0:
+        logger.error(output)
+    elif output.__contains__('\nNothing to clear.'):
+        logger.warn("DAG %s 中的 %s 无相关任务被Clear" % (dag, task))
+    else:
+        logger.info(cmd)
+
+
+@auto_batch_original()
+def main(para=None):
+    """
+    clear该dag下所有的task以及与sensor有关的task
+    :param para:
+    :return:
+    """
     dag_id = para.get('dag_id')
     task_id = para.get('task_id')
     dep_list = get_dependencies(dag_id, task_id, down=True)
+    dep_list.add(task_id)  # 添加自身，防止找不到自身sensor
     date = (datetime.datetime.now()+datetime.timedelta(days=-1)).strftime("%Y-%m-%d")  # 当前日期减一天
     # 先clear该task
     # airflow clear daily_task_online -t time.time_sensor_08_00 -d -s 2019-09-21 -e 2019-09-21
-    cmd = 'airflow clear %s -t %s -d -c -s %s -e %s' % (dag_id, task_id, date, date)
-    (status, output) = commands.getstatusoutput(cmd)
-    print(cmd)
-    if status != 0:
-        print(output)
+    """
+    -s start_date  -e end_date 时间默认是0点00分 是指task的启动时间和结束时间，如果 -s 2020-01-16 -e 2020-01-16z则任务启动时间为17日0点时才会检测到要被clear的任务
+    这种情况一旦任务不是17日0点启动，就不能被clear  这时设置-s 2020-01-16 -e 2020-01-17才能被读取到并clear
+    -c参数： 命令强制执行无需确认
+    """
+    cmd = 'airflow clear %s -t %s -d -c -s %s' % (dag_id, task_id, date)
+    run_and_return(cmd, dag_id, task_id)
     whole_dag_tasks = find_whole_tasks_and_dag()
     clear_tasks.clear()
     find_related_tasks(dep_list, whole_dag_tasks)   # 有关的sensor列表保存再clear_tasks
@@ -122,12 +149,8 @@ def main(para = None):
     for dag in clear_tasks:
         for task in clear_tasks[dag]:
             # airflow clear daily_task_online -t time.time_sensor_08_00 -d -s 2019-09-21 -e 2019-09-21
-            cmd = 'airflow clear %s -t %s -d -c -s %s -e %s' % (dag, task, date, date)
-            (status, output) = commands.getstatusoutput(cmd)
-            print(cmd)
-            if status != 0:
-                print(output)
-
+            cmd = 'airflow clear %s -t %s -d -c -s %s' % (dag, task, date)   # -c是 命令强制执行无需确认
+            run_and_return(cmd, dag_id, task_id)
 
 
 if __name__ == '__main__':
