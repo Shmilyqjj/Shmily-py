569a20116897cf7ee33fac39c4feed5e6ecf7f09
Shmilyqjj <710552907@qq.com>
2020-02-20 17:18:09 +0800

    os

--- Python_Study/BaseLearning/io_os_sys_test/os_test.py
+++ Python_Study/BaseLearning/io_os_sys_test/os_test.py
@@ -10,3 +10,6 @@ print(os.getcwd())
 print(os.getcwdu())
 print(os.chdir("C:\Users\Home-PC\PycharmProjects\Shmily-py\Python_Study\BaseLearning\io_os_sys_test"))  #  改变工作路径
 
+# os.path.getatime(file) 输出文件访问时间
+# os.path.getctime(file) 输出文件的创建时间
+# os.path.getmtime(file) 输出文件最近修改时间
