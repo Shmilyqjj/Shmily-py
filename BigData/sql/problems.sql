076cb97c46db282830e8d440595aca2e8217a360
Shmilyqjj <710552907@qq.com>
2020-03-13 15:48:08 +0800

    sql练习题  mysql

--- BigData/sql/problems.sql
+++ BigData/sql/problems.sql
@@ -46,4 +46,33 @@ select st_id,st_name,sum(score*substr(pro_type,2,2))/(max(start_time)-min(start_
 ) temp;
 
 
---2.
+--2.有商品价格表goods_price,其中id是自增主键，goods_id是商品id，cate_id是商品分类，price价格
+--create table goods_price(
+--	id int primary key auto_increment,
+--	goods_id VARCHAR(255),
+--	cate_id VARCHAR(255),
+--	price DOUBLE
+--);
+--
+--INSERT INTO goods_price(id,goods_id,cate_id,price) VALUES(1,'100000001','101',59.2);
+--INSERT INTO goods_price(id,goods_id,cate_id,price) VALUES(2,'100000002','101',34.55);
+--INSERT INTO goods_price(id,goods_id,cate_id,price) VALUES(3,'100000003','102',128.9);
+--INSERT INTO goods_price(id,goods_id,cate_id,price) VALUES(4,'100000004','102',18.5);
+--INSERT INTO goods_price(id,goods_id,cate_id,price) VALUES(5,'100000005','102',169.44);
+--INSERT INTO goods_price(id,goods_id,cate_id,price) VALUES(6,'100000006','103',77.7);
+-- （1）计算每一个商品分类下的商品数和商品平均价格
+select cate_id,count(*) as count,round(avg(price),4) from goods_price group by cate_id;
+
+
+-- （2）定义每一个商品的价格除以其所属类目的均价为该商品的价格比例，将所有商品按价格比例从小到大排序，取TOP3的商品，输出商品ID，商品价格，类目平均价格以及价格比例。
+select * from
+(
+select a.id,a.goods_id,a.price,b.avg_price as avg_price,a.price/b.avg_price as ratio from goods_price a left join
+(select cate_id,round(avg(price),4) as avg_price from goods_price group by cate_id) b on a.cate_id=b.cate_id
+)a order by ratio  limit 3;
+
+
+
+
+
+
