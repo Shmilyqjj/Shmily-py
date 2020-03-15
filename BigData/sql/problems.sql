e889c704c26529dec658e625843b27e9dc3850fd
Shmilyqjj <710552907@qq.com>
2020-03-15 21:13:20 +0800

    sql练习题

--- BigData/sql/problems.sql
+++ BigData/sql/problems.sql
@@ -271,7 +271,7 @@ select s.s_id,s.s_name,count(*) as total_course,sum(score.s_score) as total_scor
 from student s
 join score
 on s.s_id = score.s_id
-group by s.s_id,s.s_name
+group by s.s_id,s.s_name;
 
 -- 求：查询"唐"姓老师的数量
 select count(*) from teacher where t_name like '唐%';
@@ -361,7 +361,7 @@ where s_id not in
     group by s_id
     having count(s_id) =
     (select count(1) from course)
-)
+);
 
 -- 求：查询至少有一门课与学号为"01"的同学所学相同的同学的信息
 select * from student
@@ -372,7 +372,7 @@ and s_id in
     (
         select c_id from score where score.s_id = '01'
     )
-)
+);
 
 
 -- 求：查询和"01"号的同学学习的课程完全相同的其他同学的信息
@@ -399,7 +399,7 @@ where s.s_id not in
     where c.c_id = s.c_id
     and t.t_id = c.t_id
     and t.t_name = '唐光义'
-)
+);
 
 -- 求：查询两门及其以上不及格课程的同学的学号，姓名及其平均成绩
 select s.s_name,a.avg_score
@@ -408,7 +408,7 @@ join
 (
     select s_id,avg(score.s_score) as avg_score from score where s_score < 60 group by s_id having count(1) >= 2
 ) a
-on s.s_id = a.s_id
+on s.s_id = a.s_id;
 
 -- 求：检索"01"课程分数小于60，按分数降序排列的学生信息
 select s.*
@@ -433,90 +433,75 @@ SELECT a.s_id,
 MAX(CASE a.c_id WHEN '01' THEN a.s_score END ) 计网,
 MAX(CASE a.c_id WHEN '02' THEN a.s_score END ) 计组,
 MAX(CASE a.c_id WHEN '03' THEN a.s_score END ) 编程,
-avg(a.s_score),b.s_name FROM Score a JOIN Student b ON a.s_id=b.s_id GROUP BY a.s_id ORDER BY 5 DESC
+avg(a.s_score),b.s_name FROM Score a JOIN Student b ON a.s_id=b.s_id GROUP BY a.s_id ORDER BY 5 DESC;
 
----------------------------------------------下面 待练习--------------------------------------------------------
+
 
 -- 18.查询各科成绩最高分、最低分和平均分：以如下形式显示：课程ID，课程name，最高分，最低分，平均分，及格率，中等率，优良率，优秀率
 --及格为>=60，中等为：70-80，优良为：80-90，优秀为：>=90
-select a.c_id,b.c_name,MAX(s_score),MIN(s_score),ROUND(AVG(s_score),2),
-	ROUND(100*(SUM(case when a.s_score>=60 then 1 else 0 end)/SUM(case when a.s_score then 1 else 0 end)),2) as 及格率,
-	ROUND(100*(SUM(case when a.s_score>=70 and a.s_score<=80 then 1 else 0 end)/SUM(case when a.s_score then 1 else 0 end)),2) as 中等率,
-	ROUND(100*(SUM(case when a.s_score>=80 and a.s_score<=90 then 1 else 0 end)/SUM(case when a.s_score then 1 else 0 end)),2) as 优良率,
-	ROUND(100*(SUM(case when a.s_score>=90 then 1 else 0 end)/SUM(case when a.s_score then 1 else 0 end)),2) as 优秀率
-	from score a left join course b on a.c_id = b.c_id GROUP BY a.c_id,b.c_name
+select score.c_id as 课程ID,c.c_name as 课程name,max(score.s_score) as 最高分, min(score.s_score) as 最低分, avg(score.s_score) as 平均分,
+round((sum(case when score.s_score >= 60 then 1 else 0 end)/sum(case when score.s_score then 1 else 0 end)),2) as 及格率,
+round((sum(case when score.s_score >= 70 and score.s_score <= 80 then 1 else 0 end)/sum(case when score.s_score then 1 else 0 end)),2) as 中等率,
+round((sum(case when score.s_score >= 80 and score.s_score <= 90 then 1 else 0 end)/sum(case when score.s_score then 1 else 0 end)),2) as 优良率,
+round((sum(case when score.s_score >= 90 then 1 else 0 end)/sum(case when score.s_score then 1 else 0 end)),2) as 优秀率
+from score
+left join course c
+on score.c_id = c.c_id
+group by score.c_id;
 
 -- 求：按各科成绩进行排序，并显示排名
--- mysql没有rank函数
-	select a.s_id,a.c_id,
-        @i:=@i +1 as i保留排名,
-        @k:=(case when @score=a.s_score then @k else @i end) as rank不保留排名,
-        @score:=a.s_score as score
-    from (
-        select s_id,c_id,s_score from score GROUP BY s_id,c_id,s_score ORDER BY s_score DESC
-)a,(select @k:=0,@i:=0,@score:=0)s
---@k1051785839的写法
-(select * from (select
-t1.c_id,
-t1.s_score,
-(select count(distinct t2.s_score) from score t2 where t2.s_score>=t1.s_score and t2.c_id='01') rank
-FROM score t1 where t1.c_id='01'
-order by t1.s_score desc) t1)
-union
-(select * from (select
-t1.c_id,
-t1.s_score,
-(select count(distinct t2.s_score) from score t2 where t2.s_score>=t1.s_score and t2.c_id='02') rank
-FROM score t1 where t1.c_id='02'
-order by t1.s_score desc) t2)
-union
-(select * from (select
-t1.c_id,
-t1.s_score,
-(select count(distinct t2.s_score) from score t2 where t2.s_score>=t1.s_score and t2.c_id='03') rank
-FROM score t1 where t1.c_id='03'
-order by t1.s_score desc) t3)
+-- mysql没有rank函数(8.0以后有了) 用这个写法代替：select (@i:=@i+1) as rownum,s_id from student, (select @i:=0) as init;
+select s_id,c_id,s_score,@i:=@i+1 as 排名
+from score,(select @i:=0) as init
+order by s_score desc;
+
+
 -- 求：查询学生的总成绩并进行排名
-select a.s_id,
-	@i:=@i+1 as i,
-	@k:=(case when @score=a.sum_score then @k else @i end) as rank,
-	@score:=a.sum_score as score
-from (select s_id,SUM(s_score) as sum_score from score GROUP BY s_id ORDER BY sum_score DESC)a,
-	(select @k:=0,@i:=0,@score:=0)s
+select s_id,total_score,@i:=@i+1 as 排名
+from
+(
+    select s_id,sum(s_score) as total_score
+    from score
+    group by s_id
+) a,
+(select @i:=0) as init
+order by total_score desc;
+
 
 -- 求：查询不同老师所教不同课程平均分从高到低显示
+-- 注意 order by可以直接加函数 方便直接按平均分排序
+select c.t_id,t.t_name,s.c_id,avg(s.s_score) as avg
+from course c,score s,teacher t
+where c.c_id = s.c_id and t.t_id = c.t_id
+group by c.c_id
+order by avg desc;
 
-	select a.t_id,c.t_name,a.c_id,ROUND(avg(s_score),2) as avg_score from course a
-		left join score b on a.c_id=b.c_id
-		left join teacher c on a.t_id=c.t_id
-		GROUP BY a.c_id,a.t_id,c.t_name ORDER BY avg_score DESC;
+
 -- 求：查询所有课程的成绩第2名到第3名的学生信息及该课程成绩
+select d.*,c.排名,c.s_score,c.c_id from (
+select a.s_id,a.s_score,a.c_id,@i:=@i+1 as 排名 from score a,(select @i:=0)s where a.c_id='01'
+ORDER BY a.s_score DESC
+)c
+left join student d on c.s_id=d.s_id
+where 排名 BETWEEN 2 AND 3
+UNION
+select d.*,c.排名,c.s_score,c.c_id from (
+select a.s_id,a.s_score,a.c_id,@j:=@j+1 as 排名 from score a,(select @j:=0)s where a.c_id='02'
+ORDER BY a.s_score DESC
+)c
+left join student d on c.s_id=d.s_id
+where 排名 BETWEEN 2 AND 3
+UNION
+select d.*,c.排名,c.s_score,c.c_id from (
+select a.s_id,a.s_score,a.c_id,@k:=@k+1 as 排名 from score a,(select @k:=0)s where a.c_id='03'
+ORDER BY a.s_score DESC
+)c
+left join student d on c.s_id=d.s_id
+where 排名 BETWEEN 2 AND 3;
 
-			select d.*,c.排名,c.s_score,c.c_id from (
-                select a.s_id,a.s_score,a.c_id,@i:=@i+1 as 排名 from score a,(select @i:=0)s where a.c_id='01'
-								ORDER BY a.s_score DESC
-            )c
-            left join student d on c.s_id=d.s_id
-            where 排名 BETWEEN 2 AND 3
-            UNION
-            select d.*,c.排名,c.s_score,c.c_id from (
-                select a.s_id,a.s_score,a.c_id,@j:=@j+1 as 排名 from score a,(select @j:=0)s where a.c_id='02'
-								ORDER BY a.s_score DESC
-            )c
-            left join student d on c.s_id=d.s_id
-            where 排名 BETWEEN 2 AND 3
-            UNION
-            select d.*,c.排名,c.s_score,c.c_id from (
-                select a.s_id,a.s_score,a.c_id,@k:=@k+1 as 排名 from score a,(select @k:=0)s where a.c_id='03'
-								ORDER BY a.s_score DESC
-            )c
-            left join student d on c.s_id=d.s_id
-            where 排名 BETWEEN 2 AND 3;
 
 -- 求：统计各科成绩各分数段人数：课程编号,课程名称,[100-85],[85-70],[70-60],[0-60]及所占百分比
-
-
-		select distinct f.c_name,a.c_id,b.`85-100`,b.百分比,c.`70-85`,c.百分比,d.`60-70`,d.百分比,e.`0-60`,e.百分比 from score a
+select distinct f.c_name,a.c_id,b.`85-100`,b.百分比,c.`70-85`,c.百分比,d.`60-70`,d.百分比,e.`0-60`,e.百分比 from score a
 				left join (select c_id,SUM(case when s_score >85 and s_score <=100 then 1 else 0 end) as `85-100`,
 											ROUND(100*(SUM(case when s_score >85 and s_score <=100 then 1 else 0 end)/count(*)),2) as 百分比
 								from score GROUP BY c_id)b on a.c_id=b.c_id
@@ -531,16 +516,20 @@ from (select s_id,SUM(s_score) as sum_score from score GROUP BY s_id ORDER BY su
 								from score GROUP BY c_id)e on a.c_id=e.c_id
 				left join course f on a.c_id = f.c_id
 
--- 求：查询学生平均成绩及其名次
 
-		select a.s_id,
-				@i:=@i+1 as '不保留空缺排名',
-				@k:=(case when @avg_score=a.avg_s then @k else @i end) as '保留空缺排名',
-				@avg_score:=avg_s as '平均分'
-		from (select s_id,ROUND(AVG(s_score),2) as avg_s from score GROUP BY s_id ORDER BY avg_s DESC)a,(select @avg_score:=0,@i:=0,@k:=0)b;
+-- 求：查询学生平均成绩及其名次
+select a.s_id,
+@i:=@i+1 as '不保留空缺排名',
+@k:=(case when @avg_score=a.avg_s then @k else @i end) as '保留空缺排名',
+@avg_score:=avg_s as '平均分'
+from (select s_id,ROUND(AVG(s_score),2) as avg_s from score GROUP BY s_id ORDER BY avg_s DESC)a,(select @avg_score:=0,@i:=0,@k:=0)b;
+
+-- ****************************************************下面待做********************************************
 -- 求：查询各科成绩前三名的记录
-			-- 1.选出b表比a表成绩大的所有组
-			-- 2.选出比当前id成绩大的 小于三个的
+    -- 1.选出b表比a表成绩大的所有组
+    -- 2.选出比当前id成绩大的 小于三个的
+
+
 		select a.s_id,a.c_id,a.s_score from score a
 			left join score b on a.c_id = b.c_id and a.s_score<b.s_score
 			group by a.s_id,a.c_id,a.s_score HAVING COUNT(b.s_id)<3
