a05e872bab225b583707f10afa81e584af4ab155
Shmilyqjj <710552907@qq.com>
2020-04-14 17:49:44 +0800

    datediff应用

--- BigData/sql/datediff.sql
+++ BigData/sql/datediff.sql
@@ -11,4 +11,9 @@ select * from student.orderform having datediff(now(), submitTime) = 2488;
 
 select *,datediff(now(),TDate) from student.teacher;
 
+# 例子： 查询每个老师最近的一次上课天数  TNo教师编号，TDate上课时间   精确到day
+select TNo,min(datediff(now(),TDate)) from student.teacher group by TNo;
+
+select orderFormID,min(now()- cast(submitTime as DATE)) from student.orderform;  # 精确到秒
+select orderFormID,max(submitTime) from student.orderform;  # 精确到秒
 
