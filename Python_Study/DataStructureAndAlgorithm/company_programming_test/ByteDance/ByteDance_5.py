d4a03ec4e99c90f67609e593ef72f98d1598e77d
Shmilyqjj <710552907@qq.com>
2020-04-24 10:22:25 +0800

    字节笔试

---
+++ Python_Study/DataStructureAndAlgorithm/company_programming_test/ByteDance/ByteDance_5.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+:Description:
+:Author: 佳境Shmily
+:Create Time: 2020/4/22 10:20
+:File: ByteDance_5
+:Site: shmily-qjj.top
+
+算法（字典序的时候最后E出了乱序QAQ）：
+
+按原字符串字典序全排列
+
+例：ABCDE->ABCED...EDCBA
+
+版本号比较
+
+例：1.3.1>1.3.1.1
+
+找出比左边都大的数
+
+螺旋输出数组（没run通）
+
+
+
+给一组数字，这些数字里面每一个都重复出现了三次，只有一个数字只出现了一个
+
+
+
+https://blog.csdn.net/DevolperFront/article/details/99688144
+"""
