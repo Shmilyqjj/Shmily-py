38e8812ec67f38a9b2f25540673dd265a8107671
Shmilyqjj <710552907@qq.com>
2020-07-21 00:02:07 +0800

    分布式python架构ray

---
+++ BigData/learn_and_tests/ray/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+:Description: Python分布式架构ray https://github.com/ray-project/ray
+:Author: 佳境Shmily
+:Create Time: 2020/7/21 0:00
+:File: __init__.py
+:Site: shmily-qjj.top
+
+Ray与TensorFlow，PyTorch和MXNet等深度学习框架完全兼容，在许多应用中与Ray一起使用一个或多个深度学习框架是很自然的（例如，我们的强化学习库使用TensorFlow和PyTorch）。
+
+https://blog.csdn.net/lck5602/article/details/78665520
+"""
