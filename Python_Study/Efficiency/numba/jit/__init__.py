11bff5a679886088686ae80c2259c5471b0f4514
Shmilyqjj <710552907@qq.com>
2020-07-21 19:45:03 +0800

    Python高效运行编译库numba jit即时编译 提速100倍

---
+++ Python_Study/Efficiency/numba/jit/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+:Description: numba.jit解释器
+:Author: 佳境Shmily
+:Create Time: 2020/7/21 19:34
+:File: __init__.py
+:Site: shmily-qjj.top
+"""
---
+++ Python_Study/Efficiency/numba/jit/code_without_jit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+:Description: 一段代码 不使用numba的jit即时编译器
+:Author: 佳境Shmily
+:Create Time: 2020/7/21 19:35
+:File: code_without_jit
+:Site: shmily-qjj.top
+"""
+import time
+
+
+def time_com(i):
+    cum = 0
+    for test in range(i):
+        for ind in range(i):
+            cum += (test * ind) % 3
+
+
+if __name__ == '__main__':
+    t1 = time.time()
+    for i in range(1000):
+        time_com(i)
+    t2 = time.time()
+    print("run time:%f s" % (t2 - t1))
+# run time:48.882072 s
