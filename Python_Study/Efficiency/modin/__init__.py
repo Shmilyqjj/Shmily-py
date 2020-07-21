05b54cf652c2b0ec6ef49d65c911f72582fcaa5a
Shmilyqjj <710552907@qq.com>
2020-07-21 23:00:01 +0800

    modin

--- Python_Study/Efficiency/modin/__init__.py
+++ Python_Study/Efficiency/modin/__init__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-:Description: 使用modin.pandas测试
+:Description: 使用modin.pandas
 :Author: 佳境Shmily
 :Create Time: 2020/7/20 23:55
 :File: __init__.py
 :Site: shmily-qjj.top
+
+官方网站
+https://modin.readthedocs.io/en/latest/
 """
