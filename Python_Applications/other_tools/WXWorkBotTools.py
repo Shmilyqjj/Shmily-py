4177eef74fc7cfaeb32b5d870b26b31efce2e58c
Shmily <43771567+Shmilyqjj@users.noreply.github.com>
2021-10-09 15:12:10 +0800

    Update WXWorkBotTools.py

--- Python_Applications/other_tools/WXWorkBotTools.py
+++ Python_Applications/other_tools/WXWorkBotTools.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # encoding: utf-8
 """
-:Description: 企业微信机器人 工具
+:Description: 企业微信机器人
 :Author: Shmily
 :Create Time: 2021/10/9 10:47
 :File: WXWorkBotTools.py
@@ -12,7 +12,7 @@ import requests
 import hashlib
 import base64
 
-KEY = "xawdawdadawdawdawdawda"
+KEY = "7e087ab7-282e-482c-8a30-bbe461c3bb9c"
 HOOK = f"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={KEY}"
 FILES_TEMP = f"https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key={KEY}&type=file"
 HEADERS = {'Content-Type': 'application/json'}
@@ -61,7 +61,27 @@ def send_file(file_path):
     data = {"msgtype": "file", "file": {"media_id": media_id}}
     print(requests.post(url=HOOK, headers=HEADERS, json=data).json())
 
+
+def send_markdown(md_str):
+    data = """
+    {
+        "msgtype": "markdown",
+        "markdown": {
+            "content": "%s"
+        }
+   }
+    """ % md_str
+    data = str(data).encode("utf-8")
+    print(requests.post(url=HOOK, headers=HEADERS, data=data).json())
+
+
+def send_news(title, description, link_url, pic_web_url):
+    data = {"msgtype": "news", "news": {"articles": [{"title": title, "description": description, "url": link_url, "picurl": pic_web_url}]}}
+    print(data)
+    print(requests.post(url=HOOK, headers=HEADERS, json=data).json())
+
 # send_words("兄弟们，干饭了🍚 @所有人")
 # send_pics("C://Users//q00885//Downloads//大恶人.png")
 # send_file("C://Users//q00885//Sync-win//ide-eval-resetter-2.1.6.zip")
+# send_news("文东大恶人", "大恶人-文东","www.qq.com", "http://res.mail.qq.com/node/ww/wwopenmng/images/independent/doc/test_pic_msg1.png")
 
