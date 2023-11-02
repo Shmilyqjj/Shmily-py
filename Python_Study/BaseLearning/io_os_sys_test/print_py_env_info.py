7dc34f431c8e95fed1ac04d0296cedcddc72ed57
shmilyqjj <710552907@qq.com>
2023-11-02 15:44:44 +0800

    打印python运行环境的信息 python 版本和 python路径

---
+++ Python_Study/BaseLearning/io_os_sys_test/print_py_env_info.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+:Author: jiajing_qu
+:Create Time: 2020/1/21 15:30
+:@File: platform_test.py
+:@Software: PyCharm
+:@Site: shmily-qjj.top
+"""
+
+import sys, platform
+print("Python Version: " + platform.python_version() + ' Python executable: ' + sys.executable)
